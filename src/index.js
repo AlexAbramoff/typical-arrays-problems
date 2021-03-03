3bb54eb9cb43e83b8bef86008e801a782e078903
AlexAbramoff <alex93178@gmail.com>
2021-03-03 19:46:57 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let res = 0
+  if (array == undefined || array.length == 0) {
+    return res
+  } else {
+    for (i = 0; i < array.length; i++) {
+      if (array[i] < res) {
+        res = array[i]
+      }
+    }
+    return res
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let res = 0
+  if (array == undefined || array.length == 0) {
+    return res
+  } else {
+    for (i = 0; i < array.length; i++) {
+      if (array[i] > res) {
+        res = array[i]
+      }
+    }
+    return res
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let res = 0
+  if (array == undefined || array.length == 0) {
+    return res
+  } else {
+    let sum = 0
+    for (i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    res = sum / array.length
+    return res
+  }
 }
